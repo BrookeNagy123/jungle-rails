e3b29ca597a8ca56d789d6c38bd41445eea3fb58
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-05-01 18:46:18 -0600

    Add logic to authenticate user and require fields for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true 
+  validates :password, presence: true, length: {minimum: 5}
+
+  before_save :update_email 
+
+  def self.authenticate_with_credentials(email, password)
+    @email = email.strip
+    @email_lowercase = @email.downcase
+
+    @user = User.find_by_email(@email_lowercase)
+
+    if @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end 
+
+  def update_email
+    self.email = email.strip
+    self.email = self.email.downcase
+  end 
 end
